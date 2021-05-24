6a1ecd1b87db4a5f56c447de11b48b25f5cfd89e
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 07:24:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,36 +1,3 @@
-require 'pry'
-=begin
-#Try 1
-def oxford_comma(array)
-  index = 0
-  until index < array.size
-    if index == 0
-      string = array.join
-    binding.pry  
-    elsif index > 0 && index == array.size - 1
-      string = array.join("and")
-    else
-      string = array.join(", ")
-    end
-    #binding.pry
-    index += 1
-  end
-end
-
-#Try 2
-def oxford_comma(array)
-  count = 0
-  if array.size > 1 && count == array.size - 1
-    values.push array.split("and")
-    puts values
-  else 
-    values.push array.split
-    puts values
-  end
-  count += 1
-end    
-=end
-
 def oxford_comma(array)
   values = ""
   case array.size
