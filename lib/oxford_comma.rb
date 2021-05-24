22d4b525e8cafd61879c0bf6bcaf76b443939458
Utshant Gurung <utshantgurung@gmail.com>
2021-05-24 07:36:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,10 @@
 def oxford_comma(array)
-  #values = ""
   case array.size
   when 1
     values = array.join 
   when 2
     values = array.join(" and ")
   else
-    #array_last_delete = ""
     array_last_delete = array.delete(array.last)
     values = array.join(", ") + ", and " + array_last_delete
   end
